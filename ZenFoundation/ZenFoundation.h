9d69acbd2a7eac4632b95e060a8d41ee0ffadadf
Alexey Roik <alexey.roik@nebulagames.org>
2020-03-31 22:21:09 +0300

    Add empty 'Swift' project

---
+++ ZenFoundation/ZenFoundation.h
@@ -0,0 +1,19 @@
+//
+//  ZenFoundation.h
+//  ZenFoundation
+//
+//  Created by Alexey Roik on 3/31/20.
+//  Copyright © 2020 Nebula. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for ZenFoundation.
+FOUNDATION_EXPORT double ZenFoundationVersionNumber;
+
+//! Project version string for ZenFoundation.
+FOUNDATION_EXPORT const unsigned char ZenFoundationVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ZenFoundation/PublicHeader.h>
+
+
