66fb16287e9d38b7948c76fd245354660b5c33a2
Alexey Roik <alexey.roik@nebulagames.org>
2020-10-03 01:06:55 +0300

    Refacotor 'NSPredicate' extensions

---
+++ ZenFoundation/SourceCode/Extensions/Foundation/Predicate/NSPredicate+Matching.swift
@@ -0,0 +1,72 @@
+import Foundation
+
+public extension NSPredicate {
+	
+	/**
+	Returns a predicate that evaluates whether the value in specified keypath equal to the value specified as second argument.
+	- parameter keyPath: The keypath to field.
+	- parameter value: The value to compare.
+	*/
+	static func keyPath(
+		_ keyPath: String,
+		equalTo value: CVarArg
+	) -> NSPredicate {
+		if type(of: value) == String.self {
+			return NSPredicate(format: "%K == %@", keyPath, value)
+		} else {
+			return NSPredicate(format: "%K == \(value)", keyPath)
+		}
+	}
+	
+	/**
+	Returns a predicate that evaluates whether the collection contains the value in specified keypath.
+	- parameter values: The collection where to search.
+	- parameter keyPath: The keypath to field.
+	*/
+	static func values(
+		_ values: [CVarArg],
+		contains keyPath: String
+	) -> NSPredicate {
+		guard values.count == 1,
+			  let first = values.first else {
+			return NSPredicate(format: "%K IN %@", keyPath, values)
+		}
+		return self.keyPath(
+			keyPath,
+			equalTo: first
+		)
+	}
+	
+	/**
+	Returns a predicate that evaluates whether the value inmore than specified keypath is within specified range.
+	- parameter keyPath: The keypath to field.
+	- parameter range: The allowable range.
+	*/
+	static func keyPath(
+		_ keyPath: String,
+		within range: CountableClosedRange<Int>
+	) -> NSPredicate {
+		NSPredicate(
+			format: "%K >= %@ && %K <= %@",
+			keyPath, range.lowerBound,
+			keyPath, range.upperBound
+		)
+	}
+	
+	/**
+	Returns a predicate that evaluates whether the value in specified keypath is within specified range.
+	- parameter keyPath: The keypath to field.
+	- parameter range: The allowable range.
+	*/
+	static func keyPath(
+		_ keyPath: String,
+		within range: CountableRange<Int>
+	) -> NSPredicate {
+		NSPredicate(
+			format: "%K >= %@ && %K < %@",
+			keyPath, range.lowerBound,
+			keyPath, range.upperBound
+		)
+	}
+	
+}
---
+++ ZenFoundation/SourceCode/Extensions/Foundation/Predicate/NSPredicate+Relationships.swift
@@ -0,0 +1,77 @@
+import Foundation
+
+public extension NSPredicate {
+	
+	/**
+	Returns a predicate that evaluates whether the relationship contains more entities than specified count.
+	- parameter keyPath: The keypath to _to-many_ relationship.
+	- parameter count: The number to compare with entities count.
+	*/
+	static func relationship(
+		_ keyPath: String,
+		countMoreThan count: Int
+	) -> NSPredicate {
+		NSPredicate(format: "%K.@count > \(count)", keyPath)
+	}
+	
+	/**
+	Returns a predicate that evaluates whether specified field is equal to given value in all entities in specified relationship.
+	### Usage example: ###
+	```
+	let allClientsHasCardPredicate: NSPredicate = .all(
+		entityField: #keyPath(Person.hasCard),
+		equalTo: NSNumber(booleanLiteral: true).stringValue,
+		inRelationship: #keyPath(Bank.clients)
+	)
+	```
+	- parameter entityFieldKeyPath: The entity field key path.
+	- parameter fieldValue: The value to compare with entity field.
+	- parameter relationshipKeyPath: The relationship to entity (to many) key path.
+	*/
+	static func all(
+		entityField entityFieldKeyPath: String,
+		equalTo fieldValue: String,
+		inRelationship relationshipKeyPath: String
+	) -> NSPredicate {
+		NSPredicate(
+			format: "SUBQUERY("
+				+ "%K, $entity, $entity.%K == %@"
+				+ ").@count == %K.@count",
+			relationshipKeyPath,
+			entityFieldKeyPath,
+			fieldValue,
+			relationshipKeyPath
+		)
+	}
+	
+	/**
+	Returns a predicate that evaluates whether there is at least one entity with field which is equal to given value
+	in specified relationship.
+	### Usage example: ###
+	```
+	let atLeastOneJohnPredicate: NSPredicate = .all(
+		entityField: #keyPath(Person.name),
+		equalTo: "John",
+		inRelationship: #keyPath(City.citizens)
+	)
+	```
+	- parameter entityFieldKeyPath: The entity field key path.
+	- parameter fieldValue: The value to compare with entity field.
+	- parameter relationshipKeyPath: The relationship to entity (to many) key path.
+	*/
+	static func hasAtLeastOne(
+		entityField entityFieldKeyPath: String,
+		equalTo fieldValue: String,
+		inRelationship relationshipKeyPath: String
+	) -> NSPredicate {
+		NSPredicate(
+			format: "SUBQUERY("
+				+ "%K, $entity, $entity.%K == %@"
+				+ ").@count > 0",
+			relationshipKeyPath,
+			entityFieldKeyPath,
+			fieldValue
+		)
+	}
+	
+}
