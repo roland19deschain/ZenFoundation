af592346472df63bd24a9dece890c7f83815c020
Alexey Roik <roland19deschain@gmail.com>
2026-06-27 14:40:34 +0300

    Update iOS project

--- Sources/Foundation/Decimal/Decimal+Convenience.swift
+++ Sources/Foundation/Decimal/Decimal+Convenience.swift
@@ -2,6 +2,7 @@ import Foundation
 
 public extension Decimal {
 	
+	/// Returns the value of the decimal as a `Double`, with possible loss of precision.
 	var doubleValue: Double {
 		NSDecimalNumber(decimal: self).doubleValue
 	}
