c9f4f56ae07242639b0ddb9b977d10f39b90e2fa
Alexey Roik <roland19deschain@gmail.com>
2026-06-27 15:01:58 +0300

    Update iOS project

--- Sources/Foundation/Scanner/Scanner+HexadecimalNumbers.swift
+++ Sources/Foundation/Scanner/Scanner+HexadecimalNumbers.swift
@@ -4,26 +4,24 @@ public extension Scanner {
 	
 	/// Returns a Double if scanned in hexadecimal, or `nil` if not found.
 	var scanHexDouble: Double? {
-		var value: Double = 0
-		return scanHexDouble(&value) ? value : nil
+		scanDouble(representation: .hexadecimal)
 	}
 	
 	/// Returns a Float if scanned in hexadecimal, or `nil` if not found.
 	var scanHexFloat: Float? {
-		var value: Float = 0
-		return scanHexFloat(&value) ? value : nil
+		scanFloat(representation: .hexadecimal)
 	}
 	
 	/// Returns a UInt32 if scanned in hexadecimal, or `nil` if not found.
 	var scanHexInt32: UInt32? {
-		var value: UInt32 = 0
-		return scanHexInt32(&value) ? value : nil
+		scanUInt64(representation: .hexadecimal).map {
+			UInt32(clamping: $0)
+		}
 	}
 	
 	/// Returns a UInt64 if scanned in hexadecimal, or `nil` if not found.
 	var scanHexInt64: UInt64? {
-		var value: UInt64 = 0
-		return scanHexInt64(&value) ? value : nil
+		scanUInt64(representation: .hexadecimal)
 	}
 	
 }
